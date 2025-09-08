7e4ccaf69ea23b271607e49cc0eb90ac8f3b761c
Kirlosk <kirloskaraditya1@gmail.com>
2025-09-08 12:15:34 +0530

    '

--- OOPs/Operators.java
+++ OOPs/Operators.java
@@ -4,6 +4,7 @@ class Operators{
     public static void main(String []args){
         int x=10;
 
+        System.out.println("Post increment and Pre increment-->");
         System.out.println("Orignal: "+x);
         System.out.println("Post Increment: "+(x++));
         System.out.println("After post increment: "+x);
@@ -11,11 +12,21 @@ class Operators{
         System.out.println("Post-decrement: "+(x--));
         System.out.println("Pre decrement: "+(--x));
 
+        System.out.println("");
         System.out.println("------------------------------------");
+        System.out.println("");
 
+        System.out.println("And operator-->");
         int a =12;
+        System.out.println("a: "+a);
         int b=10;
+        System.out.println("b: "+b);
         int result =a&b;
         System.out.println("a & b: "+ result);
+
+        System.out.println("");
+        System.out.println("------------------------------------");
+        System.out.println("");
+        
     }
 }
