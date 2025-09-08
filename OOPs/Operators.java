626d1ebbbbf513241d6787df47cf0cae82f4e53e
Kirlosk <kirloskaraditya1@gmail.com>
2025-09-08 12:08:53 +0530

    '

---
+++ OOPs/Operators.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Operators{
+    public static void main(String []args){
+        int x=10;
+
+        System.out.println("Orignal: "+x);
+        System.out.println("Post Increment: "+(x++));
+        System.out.println("After post increment: "+x);
+        System.out.println("Pre-increment: "+(++x));
+        System.out.println("Post-decrement: "+(x--));
+        System.out.println("Pre decrement: "+(--x));
+    }
+}
