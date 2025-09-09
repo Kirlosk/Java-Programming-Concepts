85cfab973b450435fbdce571eeba5948c056eb06
Kirlosk <kirloskaraditya1@gmail.com>
2025-09-09 14:54:42 +0530

    '

---
+++ OOPs/Shift.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Shift{
+    public static void main(String []args){
+
+        System.out.println("Left shift operator -->");
+        int a =5;
+        int result=a<<2;
+        System.out.println("a << 2= "+result);
+
+        System.out.println(" ");
+        System.out.println("-------------");
+        System.out.println(" ");
+        System.out.println("Right shift operator -->");
+
+        int b =20;
+        int result_2=b>>2;
+        System.out.println("a >> 2= "+result_2);
+
+
+
+    }
+}
