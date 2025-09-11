00e4849969639c959d7ab14f538b443da3452641
Kirlosk <kirloskaraditya1@gmail.com>
2025-09-11 12:11:29 +0530

    '

--- Operators/Operators_2.java
+++ Operators/Operators_2.java
@@ -9,6 +9,16 @@ class Operators_2{
         System.out.println("--------------");
         System.out.println("Is  x>y: "+(x>y));
         System.out.println("--------------");
+        System.out.println("Logical not--->");
+
+        boolean isRaining=false;
+
+        if(!isRaining){
+            System.out.println("You can go outside without an umbrella.");
+        }
+        else{
+            System.out.println("Take an Umbrella");
+        }
 
 
     }
