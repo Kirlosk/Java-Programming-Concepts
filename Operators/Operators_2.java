03a6fc77215d22b638a5d1403b26375a0dd1088d
Kirlosk <kirloskaraditya1@gmail.com>
2025-09-11 12:00:21 +0530

    '

---
+++ Operators/Operators_2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Operators_2{
+    public static void main(String []args){
+        int x=5;
+        int y=9;
+        System.out.println("Is x<y: "+(x<y));
+
+    }
+}
