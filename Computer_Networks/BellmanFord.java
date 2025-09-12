e0ff76955cfaf09804daac95f65d643cfed5237a
Kirlosk <kirloskaraditya1@gmail.com>
2025-09-12 23:31:18 +0530

    '

--- Computer_Networks/BellmanFord.java
+++ Computer_Networks/BellmanFord.java
@@ -1,41 +1,39 @@
 import java.util.Scanner;
- public class BellmanFord{
-    public static void bellmanFord(int[][] graph, int V, int E, int src) {
+public class BellmanFord{
+    public static void bellmanFord(int[][] graph, int V, int E, int src){
         int[] dist = new int[V];
-        for (int i = 0; i < V; i++) {
+        for(int i = 0; i < V; i++){
             dist[i] = Integer.MAX_VALUE;
-        }
+         }
         dist[src] = 0;
-        for (int i = 1; i < V; i++) {
-            for (int j = 0; j < E; j++) {
+        for(int i = 1; i < V; i++){
+            for(int j = 0; j < E; j++){
                 int u = graph[j][0];
                 int v = graph[j][1];
                 int weight = graph[j][2];
-                if (dist[u] != Integer.MAX_VALUE && dist[u] + weight < dist[v]) {
+                if(dist[u] != Integer.MAX_VALUE && dist[u] + weight < dist[v]){
                     dist[v] = dist[u] + weight;
-                }
-            }
-        }
-        for (int j = 0; j < E; j++) {
+                 }
+             }
+         }
+        for(int j = 0; j < E; j++){
             int u = graph[j][0];
             int v = graph[j][1];
             int weight = graph[j][2];
-            if (dist[u] != Integer.MAX_VALUE && dist[u] + weight < dist[v]) {
+            if(dist[u] != Integer.MAX_VALUE && dist[u] + weight < dist[v]){
                 System.out.println("Graph contains negative weight cycle");
                 return;
-            }
-        }
-
-        
-        
+             }
+         }
         printSolution(dist, V);
     }
-    public static void printSolution(int[] dist, int V) {
+    public static void printSolution(int[] dist, int V){
         System.out.println("Vertex\t\tDistance from Source");
-        for (int i = 0; i < V; i++) {
-            if (dist[i] == Integer.MAX_VALUE) {
+        for(int i = 0; i < V; i++){
+            if(dist[i] == Integer.MAX_VALUE){
                 System.out.println(i + "\t\t" + "INF");
-            } else {
+            }
+            else{
                 System.out.println(i + "\t\t" + dist[i]);
             }
         }
@@ -48,15 +46,17 @@ import java.util.Scanner;
         int E = sc.nextInt();
         int[][] graph = new int[E][3];
         System.out.println("Enter the edges with source, destination, and weight:");
-        for (int i = 0; i < E; i++) {
+        for(int i = 0; i < E; i++){
             System.out.print("Edge " + (i + 1) + ": ");
             graph[i][0] = sc.nextInt();
-graph[i][1] = sc.nextInt();
- graph[i][2] = sc.nextInt();
+            graph[i][1] = sc.nextInt();
+            graph[i][2] = sc.nextInt();
+         }
+        System.out.print("Enter the source vertex: ");
+        int src = sc.nextInt();
+        bellmanFord(graph, V, E, src);
+        sc.close();
+     }
  }
- System.out.print("Enter the source vertex: ");
- int src = sc.nextInt();
- bellmanFord(graph, V, E, src);
- sc.close();
- }
- }
+
+ //Kirlosk
